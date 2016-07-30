516e18b18574049aeba4715f2307cba9efdbcec7
gnerkus <ifeanyioraelosi@gmail.com>
2016-07-30 17:49:49 +0100

    [Header] create Header container
    The Header container serves as the functional component for the Header.

---
+++ src/containers/HeaderContainer.js
@@ -0,0 +1,36 @@
+import { connect } from 'react-redux';
+import Header from '../components/Header';
+import { logout } from '../actions';
+import { history } from '../history';
+
+import C from '../constants';
+
+const mapStateToProps = (state, ownProps) => {
+  return {
+    authStatus: state.auth.authStatus,
+    location: ownProps.location,
+    photoURL: state.auth.photoURL,
+    userName: state.auth.userName,
+    email: state.auth.email
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    logout: e => {
+      console.log(e);
+      e.preventDefault();
+      C.FIREBASE.auth().signOut().then(() => {
+        dispatch(logout());
+        location.reload();
+      });
+    }
+  };
+}
+
+const HeaderContainer = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Header);
+
+export default HeaderContainer;

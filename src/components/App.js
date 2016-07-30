21eec1995e00d88d3345da5ca526b3af1eae1a2c
gnerkus <ifeanyioraelosi@gmail.com>
2016-07-30 18:51:09 +0100

    [App] define presentational and functional components for the App
    The App container serves as the functional component for the app while
    the App component serves as the presentational component.
    
    The App component contains only the Header component and renders
    children of the App obtained from the <IndexRoute> declaration in the
    router

---
+++ src/components/App.js
@@ -0,0 +1,68 @@
+import React, { PropTypes } from 'react';
+import { Link, browserHistory } from 'react-router';
+
+import C from '../constants';
+import HeaderContainer from '../containers/HeaderContainer';
+
+import CircularProgress from 'material-ui/CircularProgress';
+
+class App extends React.Component {
+
+  static propTypes {
+    return {
+      uid: PropTypes.string,
+      authStatus: PropTypes.string.isRequired,
+      userName: PropTypes.string
+    };
+  }
+
+  componentDidUpdate() {
+    componentHandler.upgradeDom();
+  }
+
+  pageContent() {
+    let content = <CircularProgress />;
+    if (!this.props.dataLoading) {
+      content = (
+        <div>
+          {
+            React.cloneElement({
+              this.props.children,
+              key: this.props.location.pathname
+            })
+          }
+        </div>
+      );
+    }
+
+    return content;
+  }
+
+  render() {
+    let content = null;
+
+    if (this.props.authStatus === C.LOGGING_IN) {
+      content = <CircularProgress />;
+    } else {
+      content = (
+        <div>
+          <HeaderContainer
+            location={ this.props.location }
+            open={ false } />
+
+          <main>
+            { this.pageContent() }
+          </main>
+
+          <footer>
+            Made with gusto by <a href="https://github.com/gnerkus">gnerkus</a>
+          </footer>
+        </div>
+      );
+    }
+
+    return content;
+  }
+}
+
+export default App;

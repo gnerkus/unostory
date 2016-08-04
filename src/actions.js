8c1d5c524a99690988c9abf3e881db09eaebe0ad
gnerkus <ifeanyioraelosi@gmail.com>
2016-08-04 15:40:57 +0100

    [Actions] refactor actions list and create 'listeningToSettings' method

--- src/actions.js
+++ src/actions.js
@@ -40,19 +40,11 @@ export const logout = (nextPath = '/') => {
   };
 };
 
-// Subscribe to changes in the user's auth state
-export const startListeningToAuth = () => {
-  return function (dispatch) {
-    dispatch(listeningToAuth());
-    // Start listening to firebase auth changes.
-    C.FIREBASE.auth().onAuthStateChanged(authData => {
-      // If logged in.
-      if (authData) {
-        dispatch(loginSuccess(authData));
-      } else {
-        dispatch(logout());
-      }
-    });
+// Action creator
+// Loaded data successfully
+export const loadedData = () => {
+  return {
+    type: C.LOADED_DATA,
   };
 };
 
@@ -65,34 +57,94 @@ export const fetchedChapters = (chapters) => {
   };
 };
 
-// Subscribe to changes in the `chapters` ref.
-export const startListeningToChapters = () => {
-  let listenAction = null;
-  if (C.FIREBASE.auth().currentUser) {
-    const uid = C.FIREBASE.auth().currentUser.uid;
-    listenAction = (dispatch) => {
-      const chaptersRef = C.FIREBASE.app()
-                                    .database()
-                                    .ref('chapters')
-                                    .equalTo('uid', uid);
+// Action creator
+// Updated settings
+export const updateSetting = (setting, value) => {
+  return {
+    type: C.UPDATE_SETTING,
+    setting,
+    value,
+  };
+};
 
-      // Whenever the `chapters` ref changes, send an action to the
-      // app's state along with the `chapters` ref.
-      chaptersRef.on('value', snapshot => {
-        console.log(snapshot.val());
-        dispatch(fetchedChapters(snapshot.val()));
-      }, err => {
-        // TODO: better error handling
-        console.log(err);
-      });
-    };
-  } else {
-    listenAction = (dispatch) => {
-      dispatch(logout());
-    };
-  }
+// Action creator
+// Fetched settings
+export const fetchedSettings = settings => {
+  return {
+    type: C.FETCHED_SETTINGS,
+    settings,
+  };
+};
+
+// Subscribe to changes in the `chapters` ref.
+export const startListeningToChapters = (user) => {
+  const listenAction = (dispatch) => {
+    const chaptersRef = C.FIREBASE.app()
+                                  .database()
+                                  .ref('chapters')
+                                  .equalTo('uid', user.uid);
+
+    // Whenever the `chapters` ref changes, send an action to the
+    // app's state along with the `chapters` ref.
+    chaptersRef.on('value', snapshot => {
+      console.log(snapshot.val());
+      dispatch(loadedData());
+      dispatch(fetchedChapters(snapshot.val()));
+    }, err => {
+      // TODO: better error handling
+      console.log(err);
+    });
+  };
 
   return listenAction;
+};
+
+// Subscribe to changes in the `setttings` ref.
+export const startListeningToSettings = (user) => {
+  const listenAction = (dispatch) => {
+    const settingsRef = C.FIREBASE.app()
+                                  .database()
+                                  .ref('settings')
+                                  .equalTo('uid', user.uid);
+
+    // Whenever the `settings` ref changes, send an action to the
+    // app's state along with the `settings` ref.
+    settingsRef.on('value', snapshot => {
+      console.log(snapshot.val());
+      dispatch(loadedData());
+      dispatch(fetchedSettings(snapshot.val()));
+    }, err => {
+      // TODO: better error handling
+      console.log(err);
+    });
+  };
+
+  return listenAction;
+};
+
+// Subscribe to changes in the user's auth state
+export const startListeningToAuth = () => {
+  return function (dispatch) {
+    // Notify store that app is listening for auth changes
+    dispatch(listeningToAuth());
+    // Start listening to firebase auth changes.
+    C.FIREBASE.auth().onAuthStateChanged(user => {
+      // If logged in.
+      if (user) {
+        dispatch(loginSuccess(user));
+        startListeningToChapters(user);
+        startListeningToSettings(user);
+      } else {
+        C.FIREBASE.auth().getRedirectResult().then(function (result) {
+          if (!result.user) {
+            dispatch(logout());
+          } else {
+            dispatch(loginSuccess(result.user));
+          }
+        });
+      }
+    });
+  };
 };
 
 // Chapter action creator
@@ -196,32 +248,5 @@ export const clearDialog = () => {
 export const loadingData = () => {
   return {
     type: C.LOADING_DATA,
-  };
-};
-
-// Action creator
-// Loaded data successfully
-export const loadedData = () => {
-  return {
-    type: C.LOADED_DATA,
-  };
-};
-
-// Action creator
-// Updated settings
-export const updateSetting = (setting, value) => {
-  return {
-    type: C.UPDATE_SETTING,
-    setting,
-    value,
-  };
-};
-
-// Action creator
-// Fetched settings
-export const fetchedSettings = settings => {
-  return {
-    type: C.FETCHED_SETTINGS,
-    settings,
   };
 };

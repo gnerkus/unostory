acc008fafda9924ba14aafe26c142db2e56534d3
gnerkus <ifeanyioraelosi@gmail.com>
2016-08-04 15:53:30 +0100

    [Theme] change app theme and refactor

--- src/theme.js
+++ src/theme.js
@@ -1,33 +1,41 @@
 import {
-  blueGrey700,
-  blueGrey800,
-  pink500,
+  cyan500,
+  cyan600,
+  orange500,
   grey100,
   grey300,
   grey400,
   grey500,
   white,
   darkBlack,
-  fullBlack
+  fullBlack,
 } from 'material-ui/styles/colors';
+import typography from 'material-ui/styles/typography';
+import spacing from 'material-ui/styles/spacing';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 
 const MUITHEME = getMuiTheme({
+  appBar: {
+    titleFontWeight: typography.fontWeightLight,
+    padding: spacing.desktopGutter,
+  },
+  spacing,
   fontFamily: 'Roboto, sans-serif',
+  fontWeight: 300,
   palette: {
-    primary1Color: blueGrey700,
-    primary2Color: blueGrey800,
+    primary1Color: cyan500,
+    primary2Color: cyan600,
     primary3Color: grey400,
-    accent1Color: pink500,
+    accent1Color: orange500,
     accent2Color: grey100,
     accent3Color: grey500,
     textColor: darkBlack,
     alternateTextColor: white,
     canvasColor: white,
     borderColor: grey300,
-    pickerHeaderColor: pink500,
+    pickerHeaderColor: orange500,
     shadowColor: fullBlack,
   },
 });
 
-export default MUITHEME
+export default MUITHEME;

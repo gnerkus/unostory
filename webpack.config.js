19c654443e90874c8a913c32574db6f0d26324d8
gnerkus <ifeanyioraelosi@gmail.com>
2016-08-10 14:12:32 +0100

    [Core] install react-markdown and json-loader
    React-markdown is required for rendering the content of a chapter to the
    page. It requires 'json-loader'

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,40 @@
-var webpack = require('webpack');
-var path = require('path');
-var srcPath = path.resolve(__dirname, 'src');
+const webpack = require('webpack');
+const path = require('path');
+const srcPath = path.resolve(__dirname, 'src');
 
 module.exports = {
   entry: {
     bundle: path.join(srcPath, 'app.js'),
-    common: ['react', 'react-router', 'redux', 'react-redux']
+    common: ['react', 'react-router', 'redux', 'react-redux'],
   },
 
   output: {
     path: path.resolve(__dirname, 'public'),
     filename: '[name].js',
-    publicPath: '/'
+    publicPath: '/',
   },
 
   resolve: {
     extensions: ['', '.js'],
-    modulesDirectories: ['node_modules', 'bower_components']
+    modulesDirectories: ['node_modules', 'bower_components'],
   },
 
   module: {
     loaders: [
       {
         loader: 'babel',
-        exclude: /(node_modules|bower_components)/
+        exclude: /(node_modules|bower_components)/,
       },
       {
         test: /\.(jpg|png)$/,
         loaders: ['url?limit=25000'],
-        include: path.resolve(__dirname, 'images')
-      }
-    ]
+        include: path.resolve(__dirname, 'images'),
+      },
+      {
+        test: /\.json$/,
+        loader: 'json',
+      },
+    ],
   },
 
   plugins: [
@@ -38,14 +42,14 @@ module.exports = {
     new webpack.optimize.OccurrenceOrderPlugin(),
     new webpack.optimize.CommonsChunkPlugin(
       {
-        name: "common",
-        minChunks: 2
+        name: 'common',
+        minChunks: 2,
       }
     ),
     new webpack.DefinePlugin({
       'process.env': {
-        'NODE_ENV': JSON.stringify('production')
-      }
-    })
-  ]
+        NODE_ENV: JSON.stringify('production'),
+      },
+    }),
+  ],
 };

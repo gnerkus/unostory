488b68adf137d9dc0bd1f37680aa9473be97bb85
gnerkus <ifeanyioraelosi@gmail.com>
2016-08-12 11:44:21 +0100

    [Chapter] add edit and delete buttons to chapters in chapter list
    Each chapter in the ChapterList component has an edit button that
    enables the user to edit the chapter. It also has a delete button that
    enables the user to delete the chapter

--- src/components/ChapterList.js
+++ src/components/ChapterList.js
@@ -1,10 +1,15 @@
 import React, { PropTypes } from 'react';
+import ReactMarkdown from 'react-markdown';
 import moment from 'moment';
 
-import { Card, CardTitle } from 'material-ui/Card';
+import { Card, CardTitle, CardText, CardActions } from 'material-ui/Card';
 import { List, ListItem } from 'material-ui/List';
 
-import { grey50 } from 'material-ui/styles/colors';
+import IconButton from 'material-ui/IconButton';
+import ActionDelete from 'material-ui/svg-icons/action/delete';
+import ContentCreate from 'material-ui/svg-icons/content/create';
+
+import { grey50, grey500 } from 'material-ui/styles/colors';
 
 import history from '../history';
 
@@ -35,11 +40,23 @@ const listStyle = {
   minWidth: 640,
 };
 
+const smallIconStyle = {
+  width: 24,
+  height: 24,
+  color: grey500,
+};
+
+const titleStyle = {
+  fontWeight: 'normal',
+};
+
 class ChapterList extends React.Component {
 
   static propTypes() {
     return {
       chapters: PropTypes.object,
+      removeChapter: PropTypes.func,
+      editChapter: PropTypes.func,
     };
   }
 
@@ -60,8 +77,36 @@ class ChapterList extends React.Component {
             >
               <CardTitle
                 title={chapter.title}
-                subtitle={moment(chapter.created).format('LLLL')}
+                subtitle={moment(chapter.created).format('MMM Do')}
+                titleStyle={titleStyle}
               />
+              <CardText>
+                <ReactMarkdown source={chapter.content.slice(0, 100)} />
+              </CardText>
+              <CardActions>
+                <IconButton
+                  tooltip="Edit chapter"
+                  iconStyle={smallIconStyle}
+                  onClick={(e) => {
+                    e.preventDefault();
+                    e.stopPropagation();
+                    this.props.editChapter(chapter, key);
+                  }}
+                >
+                  <ContentCreate />
+                </IconButton>
+                <IconButton
+                  tooltip="Delete chapter"
+                  iconStyle={smallIconStyle}
+                  onClick={(e) => {
+                    e.preventDefault();
+                    e.stopPropagation();
+                    this.props.removeChapter(key);
+                  }}
+                >
+                  <ActionDelete />
+                </IconButton>
+              </CardActions>
             </Card>
           </ListItem>
         );
--- src/containers/ChapterListContainer.js
+++ src/containers/ChapterListContainer.js
@@ -1,6 +1,11 @@
 import { connect } from 'react-redux';
 import ChapterList from '../components/ChapterList';
 
+import C from '../constants';
+import { showDialog, updateChapter, removeChapter } from '../actions';
+
+import history from '../history';
+
 const mapStateToProps = (state) => {
   return {
     chapters: state.chapters,
@@ -9,6 +14,22 @@ const mapStateToProps = (state) => {
 
 const mapDispatchToProps = (dispatch) => {
   return {
+    removeChapter: (chapterId) => {
+      dispatch(showDialog({
+        yesAction: () => {
+          const ref = C.FIREBASE.app().database().ref(`chapters/${chapterId}`);
+
+          ref.remove(() => {
+            dispatch(removeChapter(chapterId));
+          });
+        },
+        text: 'Deleted chapters cannot be retrieved. Are you sure?',
+      }));
+    },
+    editChapter: (chapter, chapterId) => {
+      dispatch(updateChapter(chapter, chapterId));
+      history.push(`/chapters/${chapterId}/edit`);
+    },
   };
 };
 
